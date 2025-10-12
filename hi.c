77b5c3eabb08aebf411c89ccd2d26ec392df7e44
Cog Sci Oliver <114320400+CogSciOliver@users.noreply.github.com>
2025-10-12 17:26:26 -0500

    Create hi.c
    Showcasing how int represent chars that make strings which are just arrays of arrays starting at position zero and ending with a \0 to signify the end.

---
+++ hi.c
@@ -0,0 +1,21 @@
+//TO RUN THIS 
+//INTERMINAL TYPE
+// make hi 
+// ./hi 
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+  string words[2];
+  words[0] = "HI!";
+  words[1] = "BYE!";
+
+  printf("%c%c%C\n", words[0][0],words[0][1],)words[0][2]);
+  printf("%c%c%c%c\n",words[1][0],words[1][1],words[1][2],words[1][3],);
+}
+
+// %c - show the english/latin letter 
+// $s - show the string = array of arrays 
+// %i - show the integer ASCII representation 
