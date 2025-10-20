6a336b2aa8e435951b15721a6e7f47b05316d384
Cog Sci Oliver <114320400+CogSciOliver@users.noreply.github.com>
2025-10-20 18:26:11 +0000

    added greet for a int main with arguments passed in

---
+++ Week2/greet.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <studio.h>
+
+int main(int argc, string argv[])
+{
+    if (int i = 0; i < argc; i++)
+    {
+        printf("hellom %s\n", argv[i]);
+    }
+}
+
+// TO RUN THIS PROGRAM 
+// IN THE TERMINAL, TYPE 
+// make greet
+// ./greet plus a string
+// THERE WILL NOT BE A PROMPT BECAUSE THE PROGRAM TAKES THE ARGUMENT AS A PROPERTY 
