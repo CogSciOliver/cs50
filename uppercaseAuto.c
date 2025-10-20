5f85c9def97bfee6ae77b32bfa863780f9f1c943
Cog Sci Oliver <114320400+CogSciOliver@users.noreply.github.com>
2025-10-20 13:13:10 -0500

    Create uppercaseAuto.c for string uppercasing
    This program takes a string input and converts it to uppercase.

---
+++ uppercaseAuto.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <studio.h>
+#include <string.h>
+
+int main(void)
+{
+  string s = get_string("Before: ");
+  printf("After:  ");
+  for (int i = 0, n = strlen(s); i < n; i++)
+  {
+      printf("%c", toupper(s[i])); //Auto generate removed all extra code from uppercaseManual.c 
+  }
+  printf("\n");
+}
+
+// TO RUN THIS PROGRAM 
+// IN THE TERMINAL, TYPE 
+// make uppercaseAuto
+// ./uppercaseAuto
+// TYPE A STRING IN THE PROMPT "Before"
