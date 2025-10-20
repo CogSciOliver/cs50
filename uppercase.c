29f3c64d07f8b8b7bb6e5dd84091eadf84d955af
Cog Sci Oliver <114320400+CogSciOliver@users.noreply.github.com>
2025-10-20 13:03:10 -0500

    Implement manual uppercase conversion for string
    Convert lowercase letters to uppercase by adjusting ASCII values.

--- uppercase.c
+++ uppercase.c
@@ -1,1 +1,24 @@
+#include <cs50.h>
+#include <studio.h>
+#include <string.h>
 
+int main(void)
+{
+  string s = get_string("Before: ");
+  printf("After:  ");
+  for (int i = 0, n = strlen(s); i < n; i++)
+  {
+    if (s[i] >= 'a' && <= 'z')
+    {
+      //Chang the letter to uppercase manually by subtracting 32 from the ascii int representation 
+      // all letters are 32 distanced from upper and lower cases. 
+      printf("%c", s[i] - 32);
+    }
+    else
+    {
+      //Print s[i]
+      printf("%c", s[i]);
+    }
+  }
+  printf("\n");
+}
